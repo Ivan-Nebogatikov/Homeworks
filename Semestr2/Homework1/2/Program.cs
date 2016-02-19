75b72e5f627b2fc35f4a4b1dec44a1bc1d69fee3
Ivan <moolush671@gmail.com>
2016-02-19 22:53:58 +0300

    Problem 2
    Solution for Problem 2 in Homework 1

---
+++ Semestr2/Homework1/2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem2
+{
+    // Class for calculating fibonacci number
+    class Program
+    {
+        // Main method for calculating
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            long number = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("{0}-е  число Фибоначчи: {1}", number, Fibonacci(number));
+        }
+
+        static private long Fibonacci(long number)
+        {
+            long curNum = 1;
+            long preNum = 1;
+            for (long i = 3; i <= number; ++i)
+            {
+                curNum += preNum;
+                preNum -= curNum;
+                preNum = -preNum;
+            }
+            return curNum;
+        }
+    }
+}

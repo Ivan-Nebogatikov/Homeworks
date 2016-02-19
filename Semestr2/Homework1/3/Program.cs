4422e0dcc403590a0f7a97cfc538d9e598bff398
Ivan <moolush671@gmail.com>
2016-02-19 23:30:44 +0300

    Problem 3
    Added solution for problem 3

---
+++ Semestr2/Homework1/3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Problem3
+{
+    // Class for sorting array
+    class Program
+    {
+        const int amountOfNumber = 5;
+
+        // Main method for program
+        static void Main(string[] args)
+        {
+            int[] array = new int[amountOfNumber] { 1, 2, 0, 3, -3 };
+            SortArray(array);
+            foreach (int element in array)
+            {
+                Console.Write("{0} ", element);
+            }
+            Console.WriteLine();
+        }
+
+        static private void SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                for (int j = array.Length - 1; j > i; --j)
+                {
+                    if (array[i] > array[j])
+                        Swap(ref array[i], ref array[j]);
+                }
+            }
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+        }
+    }
+}

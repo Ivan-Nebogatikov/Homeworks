44d7a0bca3474109d5b6bc118c0c8404cffedec3
Ivan <moolush671@gmail.com>
2016-03-12 23:49:20 +0300

    Solution for Problem 4
    Added solution for problem 4 Homework 5

---
+++ Semestr2/Homework5/4/CursorManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Problem4
+{
+    /// <summary>
+    /// Cursor manager class
+    /// </summary>
+    public class CursorManager
+    {
+        /// <summary>
+        /// Action for up arrow pressed
+        /// </summary>
+        /// <param name="sender"> Parent object </param>
+        /// <param name="args"> Action parameters </param>
+        public void OnUp(object sender, EventArgs args) =>
+            Console.CursorTop = (Console.CursorTop <= 0) ? 0 : Console.CursorTop - 1;
+
+        /// <summary>
+        /// Action for right arrow pressed
+        /// </summary>
+        /// <param name="sender"> Parent object </param>
+        /// <param name="args"> Action parameters </param>
+        public void OnRight(object sender, EventArgs args) =>
+            Console.CursorLeft = (Console.CursorLeft >= Console.WindowWidth - 1) ? Console.WindowWidth - 1 : Console.CursorLeft + 1;
+
+        /// <summary>
+        /// Action for down arrow pressed
+        /// </summary>
+        /// <param name="sender"> Parent object </param>
+        /// <param name="args"> Action parameters </param>
+        public void OnDown(object sender, EventArgs args) =>
+            Console.CursorTop = (Console.CursorTop >= Console.WindowHeight - 1) ? Console.WindowHeight - 1 : Console.CursorTop + 1;
+
+        /// <summary>
+        /// Action for left arrow pressed
+        /// </summary>
+        /// <param name="sender"> Parent object </param>
+        /// <param name="args"> Action parameters </param>
+        public void OnLeft(object sender, EventArgs args) =>
+            Console.CursorLeft = (Console.CursorLeft <= 0) ? 0 : Console.CursorLeft - 1;
+    }
+}
---
+++ Semestr2/Homework5/4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem4
+{
+    /// <summary>
+    /// Main program class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main program method
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            var cursorManager = new CursorManager();
+            eventLoop.UpHandler += cursorManager.OnUp;
+            eventLoop.RightHandler += cursorManager.OnRight;
+            eventLoop.DownHandler += cursorManager.OnDown;
+            eventLoop.LeftHandler += cursorManager.OnLeft;
+            eventLoop.Run();
+        }
+    }
+}

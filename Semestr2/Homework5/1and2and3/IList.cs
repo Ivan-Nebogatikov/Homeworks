72f983e1ef82415c40fecb23b3651f4f640af916
Ivan <moolush671@gmail.com>
2016-03-12 20:44:51 +0300

    Solutions for problems 1,2,3
    Added solutions for problems 1,2,3 form Homework 5

---
+++ Semestr2/Homework5/1and2and3/IList.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem1and2and3
+{
+    /// <summary>
+    /// Interface for work with List
+    /// </summary>
+    interface IList
+    {
+        /// <summary>
+        /// Add new elemet to list
+        /// </summary>
+        /// <param name="newElement"> New Element to list </param>
+        void Add(int newElement);
+
+        /// <summary>
+        /// Returns first index of element in list
+        /// </summary>
+        /// <param name="element"> Element value </param>
+        /// <returns> >=0 if element exist; "-1" if not found </returns>
+        int IndexOf(int element);
+
+        /// <summary>
+        /// Remove element from list
+        /// </summary>
+        /// <param name="element"> Element value to remove </param>s
+        void Remove(int element);
+
+        /// <summary>
+        /// Returns list size
+        /// </summary>
+        /// <returns> Size </returns>
+        int GetLength();
+
+        /// <summary>
+        /// Returns element in list. Numeration from 0
+        /// </summary>
+        /// <param name="index"> Index of element </param>
+        /// <returns> Int value or OutOfBoundsOfListException if index is bigger than list size </returns>
+        int GetElement(int index);
+
+        /// <summary>
+        /// Set new value to list element
+        /// </summary>
+        /// <param name="index"> Index of element </param>
+        /// <param name="value"> New element value </param>
+        /// <returns></returns>
+        void SetNewElementValue(int index, int value);
+    }
+}
---
+++ Semestr2/Homework5/1and2and3Tests/FunctionsTests.cs
@@ -0,0 +1,51 @@
+﻿using Problem1and2and3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem1and2and3.Tests
+{
+    [TestClass()]
+    public class FunctionsTests
+    {
+        private List list;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            list = new List();
+        }
+
+        [TestMethod()]
+        public void MapTest()
+        {
+            list.Add(3);
+            list.Add(30);
+            list.Add(-8);
+            list = Functions.Map(list, x => x * 2);
+            Assert.AreEqual(-16, list.GetElement(0));
+            Assert.AreEqual(60, list.GetElement(1));
+            Assert.AreEqual(6, list.GetElement(2));
+        }
+
+        [TestMethod()]
+        public void FilterTest()
+        {
+            list.Add(0);
+            list.Add(50);
+            list.Add(-2);
+            list.Add(-9999);
+            list = Functions.Filter(list, x => x >= 0);
+            Assert.AreEqual(2, list.GetLength());
+            Assert.AreEqual(0, list.GetElement(0));
+            Assert.AreEqual(50, list.GetElement(1));
+        }
+
+        [TestMethod()]
+        public void FoldTest()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.AreEqual(6, Functions.Fold(list, 1, (acc, elem) => acc * elem));
+        }
+    }
+}

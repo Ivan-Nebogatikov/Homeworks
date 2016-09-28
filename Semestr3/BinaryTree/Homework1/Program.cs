146eb3f533104a3fa7af0ce9ccce5f26518651bd
Ivan <moolush671@gmail.com>
2016-09-28 16:50:23 +0300

    Homework 1
    Added solution for homework 1

---
+++ Semestr3/BinaryTree/Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework1
+{
+    /// <summary>
+    /// Main program class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main program method
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            var tree = new BinaryTree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(6);
+            tree.Print();
+            tree.Delete(6);
+            tree.Print();
+            Console.WriteLine(tree.Contains(6));
+        }
+    }
+}

54817075d3af1b9b52f2d05c51d3d8deb2bb3f3b
Ivan <moolush671@gmail.com>
2016-12-10 23:12:05 +0300

    Updated StateManager and Drawing

--- Semestr3/Homework4/Homework4/MainForm.cs
+++ Semestr3/Homework4/Homework4/MainForm.cs
@@ -1,40 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Homework4
 {
+    /// <summary>
+    /// Main form class
+    /// </summary>
     public partial class MainForm : Form
     {
-        private readonly Pen pen = new Pen(Color.Black, 2);
-        private readonly Graphics graphics;
         private bool isDrawing;
         private Point firstCoordinate;
-        private List<Tuple<Point, Point>> lines = new List<Tuple<Point, Point>>();
         private StateManager stateManager = new StateManager();
 
         public MainForm()
         {
             InitializeComponent();
-            graphics = canvas.CreateGraphics();
-            stateManager.States.Push(new List<Tuple<Point, Point>>()); // Intitial state
         }
 
         private void CanvasPaint(object sender, PaintEventArgs e)
         {
-            canvas.Refresh();
+            canvas.Invalidate();
             var currentPoint = PointToClient(Cursor.Position);
             currentPoint.X -= canvas.Left;
             currentPoint.Y -= canvas.Top;
             if (isDrawing)
-                graphics.DrawLine(pen, firstCoordinate.X, firstCoordinate.Y,
-                    currentPoint.X, currentPoint.Y);
-            foreach (var line in lines)
-            {
-                graphics.DrawLine(pen, line.Item1.X, line.Item1.Y, line.Item2.X, line.Item2.Y);
-            }
+                new Line(firstCoordinate, currentPoint).Draw(e.Graphics);
+            stateManager.DrawCurrentState(e.Graphics);
         }
 
         private void CanvasClick(object sender, EventArgs e)
@@ -56,12 +48,11 @@ namespace Homework4
                 var secondCoordinate = PointToClient(Cursor.Position);
                 secondCoordinate.X -= canvas.Left;
                 secondCoordinate.Y -= canvas.Top;
-                lines.Add(new Tuple<Point, Point>(firstCoordinate, secondCoordinate));
                 choosingLineComboBox.Items.Add("Линия " + (choosingLineComboBox.Items.Count + 1));
-                stateManager.States.Push(lines.ToList());
+                stateManager.PushLine(new Line(firstCoordinate, secondCoordinate));
                 undoButton.Enabled = true;
                 RedoButton.Enabled = false;
-                stateManager.Redo.Clear();
+                stateManager.RedoClear();
             }
         }
 
@@ -81,11 +72,12 @@ namespace Homework4
             }
             messageLabel.Text = @"Меняйте " + (firstEndButton.Checked ? firstEndButton : secondEndButton).Text + @"-й конец Линии №" + (choosingLineComboBox.SelectedIndex + 1);
             isDrawing = true;
-            var ne = lines[choosingLineComboBox.SelectedIndex];
-            lines.RemoveAt(choosingLineComboBox.SelectedIndex);
+            var state = stateManager.GetCurrentState();
+            var line = state[choosingLineComboBox.SelectedIndex];
+            stateManager.RemoveLine(choosingLineComboBox.SelectedIndex);
             choosingLineComboBox.Items.RemoveAt(choosingLineComboBox.Items.Count - 1);
             SetItemsState(false);
-            firstCoordinate = firstEndButton.Checked ? ne.Item1 : ne.Item2;
+            firstCoordinate = firstEndButton.Checked ? line.Begin : line.End;
         }
 
         private void SetItemsState(bool state)
@@ -98,40 +90,34 @@ namespace Homework4
 
         private void UndoButtonClick(object sender, EventArgs e)
         {
-            if (stateManager.States.Count > 1)
+            if (!stateManager.IsStatesEmpty())
             {
-                var temp = stateManager.States.Pop();
-                stateManager.Redo.Push(temp);
+                stateManager.Undo();
                 RedoButton.Enabled = true;
-                temp = stateManager.States.Pop();
-                lines = temp;
-                stateManager.States.Push(temp.ToList());
-                if (stateManager.States.Count <= 1)
+                if (stateManager.IsStatesEmpty())
                     undoButton.Enabled = false;
                 ComboBoxChanging();
             }
             else
             {
-                lines.Clear();
+                stateManager = new StateManager();
                 undoButton.Enabled = false;
             }
         }
 
         private void RedoButtonClick(object sender, EventArgs e)
         {
-            var temp = stateManager.Redo.Pop();
-            stateManager.States.Push(temp.ToList());
-            lines = temp;
+            stateManager.Redo();
             undoButton.Enabled = true;
             ComboBoxChanging();
-            if (stateManager.Redo.Count == 0)
+            if (stateManager.IsRedoEmpty())
                 RedoButton.Enabled = false;
         }
 
         private void ComboBoxChanging()
         {
             choosingLineComboBox.Items.Clear();
-            for (int i = 1; i <= lines.Count; ++i)
+            for (int i = 1; i <= stateManager.GetCurrentState().Count; ++i)
             {
                 choosingLineComboBox.Items.Add("Линия" + i);
             }
--- Semestr3/Homework4/Homework4/StateManager.cs
+++ Semestr3/Homework4/Homework4/StateManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 
 namespace Homework4
@@ -9,23 +8,102 @@ namespace Homework4
     /// </summary>
     public class StateManager
     {
-        /// <summary>
-        /// Stack of program states
-        /// </summary>
-        public Stack<List<Tuple<Point, Point>>> States { get; set; }
+        private readonly Stack<List<Line>> states;
 
-        /// <summary>
-        /// Stack for redo actions
-        /// </summary>
-        public Stack<List<Tuple<Point, Point>>> Redo { get; set; }
+        private List<Line> currentState;
+
+        private readonly Stack<List<Line>> redo;
+
+        private bool changingLine = false;
 
         /// <summary>
         /// Class constructor
         /// </summary>
         public StateManager()
         {
-            States = new Stack<List<Tuple<Point, Point>>>();
-            Redo = new Stack<List<Tuple<Point, Point>>>();
+            states = new Stack<List<Line>>();
+            redo = new Stack<List<Line>>();
+            currentState = new List<Line>();
+        }
+
+        /// <summary>
+        /// Current set of lines
+        /// </summary>
+        /// <returns> List of current lines </returns>
+        public List<Line> GetCurrentState() => currentState;
+
+        /// <summary>
+        /// Remove special line from current state for changing its position
+        /// </summary>
+        /// <param name="number"> Number of this line </param>
+        public void RemoveLine(int number)
+        {
+            states.Push(new List<Line>(currentState));
+            currentState.RemoveAt(number);
+            changingLine = true;
+        }
+
+        /// <summary>
+        /// Checks is states collection empty
+        /// </summary>
+        /// <returns> True if empty </returns>
+        public bool IsStatesEmpty() => states.Count == 0;
+
+        /// <summary>
+        /// Adding new line
+        /// </summary>
+        /// <param name="newLine"> Line to adding </param>
+        public void PushLine(Line newLine)
+        {
+            if (!changingLine)
+            {
+                states.Push(new List<Line>(currentState));
+            }
+            currentState.Add(newLine);
+            changingLine = false;
+        }
+
+        /// <summary>
+        /// Clear redo collection
+        /// </summary>
+        public void RedoClear() =>
+            redo.Clear();
+
+        /// <summary>
+        /// Do redo
+        /// </summary>
+        public void Redo()
+        {
+            states.Push(new List<Line>(currentState));
+            currentState = new List<Line>(redo.Pop());
+        }
+
+        /// <summary>
+        /// Do undo
+        /// </summary>
+        public void Undo()
+        {
+            redo.Push(new List<Line>(currentState));
+            currentState = new List<Line>(states.Pop());
+        }
+
+        /// <summary>
+        /// Checks is redo collection empty
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRedoEmpty() =>
+            redo.Count == 0;
+
+        /// <summary>
+        /// Draw all lines at the current state
+        /// </summary>
+        /// <param name="graphics"> Graphics for painting </param>
+        public void DrawCurrentState(Graphics graphics)
+        {
+            foreach (var line in currentState)
+            {
+                line.Draw(graphics);
+            }
         }
     }
 }

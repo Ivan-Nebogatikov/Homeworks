0ac5d75c6f147d8586427b4e6945f0d285b92ec0
Ivan <moolush671@gmail.com>
2017-02-20 21:44:53 +0300

    Fixed merging bug

--- NextHomework/Homework1/ListCreation/Program.fs
+++ NextHomework/Homework1/ListCreation/Program.fs
@@ -1,4 +1,5 @@
 ﻿/// Creation list function
+let listCreator m n = List.filter (fun x -> x >= pown 2 m) (List.init (n + 1) (fun i -> pown 2 i))
 
 [<EntryPoint>]
 printfn "Созданный список: %A" (listCreator 2 20)

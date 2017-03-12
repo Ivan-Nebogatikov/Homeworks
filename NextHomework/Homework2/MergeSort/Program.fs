0f986a01ff0f5c7715d0793b0f4484950ec2c698
Ivan <moolush671@gmail.com>
2017-03-12 09:59:52 +0300

    Improved merge sort

--- NextHomework/Homework2/MergeSort/Program.fs
+++ NextHomework/Homework2/MergeSort/Program.fs
@@ -2,10 +2,10 @@
 let rec mergeSort list = 
     let rec merge firstList secondList acc = 
         match firstList, secondList with
-        | [], [] -> acc
-        | head::tail, [] | [], head::tail -> merge tail [] (acc @ [head])
-        | fHead::fTail, sHead::sTail when fHead < sHead -> merge fTail  secondList (acc @ [fHead])
-        | fHead::fTail, sHead::sTail -> merge firstList  sTail (acc @ [sHead])
+        | [], [] -> List.rev acc
+        | head::tail, [] | [], head::tail -> merge tail [] (head :: acc)
+        | fHead::fTail, sHead::sTail when fHead < sHead -> merge fTail  secondList (fHead :: acc)
+        | fHead::fTail, sHead::sTail -> merge firstList  sTail (sHead :: acc)
     match list with
     | [] -> list
     | [a] -> list
@@ -16,5 +16,5 @@ let rec mergeSort list =
 [<EntryPoint>]
 printfn "%A" (mergeSort [1; 3; 2; 4])
 printfn "%A" (mergeSort [-5; 10; 0; 7; 6; -8; 7804])
-let bigList = List.init 10000 (fun i -> -i)
+let bigList = List.init 20000 (fun i -> -i)
 printfn "%A" <| ((mergeSort bigList) = (List.rev bigList))

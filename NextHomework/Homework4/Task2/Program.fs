29cb842364d38237958d69398ec980dd56f9e7a7
Ivan <moolush671@gmail.com>
2017-03-18 22:50:34 +0300

    Homework 4
    Added solutions for all homework 4 problems

---
+++ NextHomework/Homework4/Task2/Program.fs
@@ -0,0 +1,17 @@
+﻿open FsCheck
+open FsUnit
+
+type Tree<'a> = 
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let rec treeMap func tree =  
+    match tree with
+    | Tree.Tip a -> Tree.Tip <| func a
+    | Tree.Node (a, left, right) -> 
+        Tree.Node (func a, treeMap func left, treeMap func right)
+
+[<EntryPoint>]
+treeMap ((+) 1) <| Node (12, Node (2, Tip 1, Tip 5), Tip 0) |> should equal <| Node (13, Node (3, Tip 2, Tip 6), Tip 1)
+Check.Quick (fun y-> (treeMap (fun x-> x * x) y = treeMap (fun x -> pown x 2) y))
+Check.Quick (fun y-> (treeMap (fun x-> x + x) y = treeMap ((*) 2) y))
---
+++ NextHomework/Homework4/Task4/Program.fs
@@ -0,0 +1,11 @@
+﻿let isPrime x = 
+    let devisors = seq { 2 .. int(sqrt <| float(x))}
+    Seq.forall (fun y -> x % y <> 0) devisors
+
+let primes =
+    Seq.initInfinite (fun i -> i + 2) // Need to skip 0 and 1 for isPrime
+    |> Seq.map (fun i -> i)
+    |> Seq.filter isPrime
+
+[<EntryPoint>]
+printfn "%A" (Seq.toList (Seq.take 1000 primes))

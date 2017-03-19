d8cee7dbc96a3093944d634df74290d95ae9b043
Ivan <moolush671@gmail.com>
2017-03-19 10:40:07 +0300

    Tests fixed

--- NextHomework/Homework4/Task2/Program.fs
+++ NextHomework/Homework4/Task2/Program.fs
@@ -1,4 +1,4 @@
-﻿open FsCheck
+﻿open NUnit.Framework
 open FsUnit
 
 type Tree<'a> = 
@@ -11,7 +11,14 @@ let rec treeMap func tree =
     | Tree.Node (a, left, right) -> 
         Tree.Node (func a, treeMap func left, treeMap func right)
 
-[<EntryPoint>]
-treeMap ((+) 1) <| Node (12, Node (2, Tip 1, Tip 5), Tip 0) |> should equal <| Node (13, Node (3, Tip 2, Tip 6), Tip 1)
-Check.Quick (fun y-> (treeMap (fun x-> x * x) y = treeMap (fun x -> pown x 2) y))
-Check.Quick (fun y-> (treeMap (fun x-> x + x) y = treeMap ((*) 2) y))
+[<Test>]
+let ``Adding 1 to all nodes`` () = treeMap ((+) 1) <| Node (12, Node (2, Tip 1, Tip 5), Tip 0) |> should equal <| Node (13, Node (3, Tip 2, Tip 6), Tip 1)
+
+[<Test>]
+let ``Set 0 to all nodes`` () = treeMap ((*) 0) <| Node (12, Tip 4, Tip -79) |> should equal <| Node (0, Tip 0, Tip 0)
+
+[<Test>]
+let ``Id func`` () = treeMap (fun x -> x) <| Node (2, Tip 4, Node(0, Tip 1, Tip 3)) |> should equal <| Node (2, Tip 4, Node(0, Tip 1, Tip 3))
+
+[<Test>]
+let ``Tip tree`` () = treeMap (fun x -> x * x) <| Tip 4 |> should equal <| Tip 16
--- NextHomework/Homework4/Task4/Program.fs
+++ NextHomework/Homework4/Task4/Program.fs
@@ -1,11 +1,24 @@
-﻿let isPrime x = 
-    let devisors = seq { 2 .. int(sqrt <| float(x))}
-    Seq.forall (fun y -> x % y <> 0) devisors
+﻿open NUnit.Framework
+open FsUnit
+open FsCheck
+
+let isPrime x = 
+    let divisors = seq { 2 .. int(sqrt <| float(x)) }
+    Seq.forall (fun y -> x % y <> 0) divisors
 
 let primes =
-    Seq.initInfinite (fun i -> i + 2) // Need to skip 0 and 1 for isPrime
-    |> Seq.map (fun i -> i)
-    |> Seq.filter isPrime
+    Seq.initInfinite (fun i -> i + 2) |> Seq.filter isPrime
+
+let listOfPrimes x =  Seq.toList <| Seq.take (int x) primes
+    
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should be unique)
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should be ascending)
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should not' (contain 6))
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should not' (contain 12))
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should not' (contain 100))
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should not' (contain 4))
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should not' (contain 26))
+Check.Quick (fun (x:uint32) -> listOfPrimes x |> should not' (contain 49))
 
 [<EntryPoint>]
 printfn "%A" (Seq.toList (Seq.take 1000 primes))

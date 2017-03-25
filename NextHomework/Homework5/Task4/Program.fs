7140561153e5f7b1c4069dd0f5a5d7ce964dc01d
Ivan <moolush671@gmail.com>
2017-03-25 21:25:06 +0300

    Added solution for task 4

---
+++ NextHomework/Homework5/Task4/Program.fs
@@ -0,0 +1,40 @@
+﻿module Program
+
+type Variable = string
+
+type Term =
+    | Var of Variable
+    | Appl of Term * Term
+    | Abstr of Variable * Term
+
+/// Support function for applications
+let rec applReduction a b = 
+    match a with
+    | Var (var) ->  Appl(a, b)
+    | Abstr (x, l) -> 
+        let rec recSubstitution expr = 
+            match expr with
+            | Var (var) -> 
+                if var = x then
+                    b
+                else
+                    expr
+            | Appl (f, s) -> Appl ((recSubstitution f), (recSubstitution s))
+            | Abstr (f, s) -> Abstr (f, (recSubstitution s))
+        recSubstitution l
+    | Appl (x, l) -> 
+        Appl(applReduction x b, l)
+
+/// Main function for beta reduction
+let rec reduction term =
+    match term with
+    | Var (var) -> term
+    | Appl (x, t) -> 
+        match x with 
+        | Var (v) -> term
+        | _ -> reduction <| applReduction x t
+    | Abstr (a, b) -> term
+
+/// Расходящийся комбинатор - зависает как и должен
+[<EntryPoint>]
+printfn "%A" <| reduction (Appl(Abstr("x", Appl(Var("x"), Var("x"))), Abstr("x", Appl(Var("x"), Var("x")))))

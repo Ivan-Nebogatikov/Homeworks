47d00ec573931d492c2e746fb16a46d0e112432a
Ivan <moolush671@gmail.com>
2017-04-11 10:04:16 +0300

    First version without tests

---
+++ NextHomework/Homework7/Task1/Program.fs
@@ -0,0 +1,19 @@
+﻿let roundHelp (x : float) (n : int) = System.Math.Round(x, n)
+
+type WorkflowBuilder (n) = 
+    member this.Bind (x, next) = 
+        next <| roundHelp x n
+
+    member this.Return (x) =
+        roundHelp x n 
+
+let rounding n = WorkflowBuilder(n)
+
+let test = 
+    rounding 3 {
+    let! a = 2.0 / 12.0
+    let! b = 3.5
+    return a / b
+    }
+
+printfn "%A" test

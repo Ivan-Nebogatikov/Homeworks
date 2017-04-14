2e4f79dec153f0b233015317d1d45a1461efd0a5
Ivan <moolush671@gmail.com>
2017-04-14 12:50:57 +0300

    Solution for test

---
+++ NextHomework/Test/Task1/Program.fs
@@ -0,0 +1,11 @@
+﻿module Program
+
+/// Helper function for generate subsequence
+let seqGenerator n = Seq.init n (fun i -> n)
+
+/// Sequence from task
+let seqResult = Seq.concat <| Seq.initInfinite (fun n -> seqGenerator n) 
+
+seqResult |> Seq.take 50 |> Seq.iter (fun item -> printf "%A " item)
+
+    

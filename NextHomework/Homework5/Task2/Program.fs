f3223d1046033a613b1ac174ab63012b7cdc07f4
Ivan <moolush671@gmail.com>
2017-04-28 20:40:21 +0300

    Fixed exceptons and add steps for point free calculations

--- NextHomework/Homework5/Task2/Program.fs
+++ NextHomework/Homework5/Task2/Program.fs
@@ -1,5 +1,11 @@
 ﻿module Program
-    
-let func = (*) >> List.map
 
 let funcDefault x l = List.map (fun y -> y * x) l
+
+let func1 x = List.map (fun y -> y * x)
+
+let func2 x = List.map ((*) x)
+
+let func3 x = List.map (*) x
+
+let func = List.map << (*)
--- NextHomework/Homework5/Task3/Program.fs
+++ NextHomework/Homework5/Task3/Program.fs
@@ -6,50 +6,69 @@ open System.IO
 /// Main function for user input
 let rec userInterface dictionary =
     printfn ""
-    let input = Console.ReadLine() |> int
-    match input with
-    | 2 -> 
-        printf "Введите имя: "
-        let name = Console.ReadLine()
-        printf "Введите номер: "
-        let number = Console.ReadLine()
-        userInterface ((name, number)::dictionary)
-    | 3 ->
-        printf "Введите имя для поиска: "
-        let name = Console.ReadLine()
-        printfn "Список его номеров: "
-        List.iter (printfn "%s" << snd) <| List.filter (fun x -> fst x = name) dictionary
+    printfn "2 - добавить запись (имя и телефон)"
+    printfn "3 - найти телефон по имени"
+    printfn "4 - найти имя по телефону"
+    printfn "5 - вывести всё текущее содержимое базы"
+    printfn "6 - сохранить текущие данные в файл"
+    printfn "7 - считать данные из файла"
+    let input = Console.ReadLine()
+    let success, result = System.Int32.TryParse(input)
+    if not success then
+        printfn "Неверный ввод!"
         userInterface dictionary
-    | 4 ->
-        printf "Введите номер для поиска: "
-        let number = Console.ReadLine()
-        printfn "Список его имен: "
-        List.iter (printfn "%s" << fst) <| List.filter (fun x -> snd x = number) dictionary
-        userInterface dictionary
-    | 5 ->
-        printfn "Вся база: "
-        List.iter (fun x -> printfn "%s: %s" (fst x) (snd x)) dictionary
-        userInterface dictionary
-    | 6 -> 
-        use writer = new StreamWriter(File.OpenWrite("Dictionary.txt"))
-        List.iter (fun (x, y) -> writer.WriteLine(x + " " + y)) dictionary
-        writer.Close()
-        printfn "Сохранено в файл"
-        userInterface dictionary     
-    | 7 -> 
-        use reader = new StreamReader(File.OpenRead("Dictionary.txt"))
-        let rec newDictionary acc = 
-            if (not reader.EndOfStream) then 
-                let pair = reader.ReadLine().Split[| |]
-                newDictionary ((pair.[0], pair.[1])::acc)
-            else
-                acc
-        let newDict = newDictionary [] 
-        reader.Close()
-        printfn "Загружено из файла"
-        userInterface newDict
-    | _ -> 
-        printfn "Заходите ещё"
+    else
+        match result with
+        | 2 -> 
+            printf "Введите имя: "
+            let name = Console.ReadLine()
+            printf "Введите номер: "
+            let number = Console.ReadLine()
+            userInterface ((name, number)::dictionary)
+        | 3 ->
+            printf "Введите имя для поиска: "
+            let name = Console.ReadLine()
+            printfn "Список его номеров: "
+            List.iter (printfn "%s" << snd) <| List.filter (fun x -> fst x = name) dictionary
+            userInterface dictionary
+        | 4 ->
+            printf "Введите номер для поиска: "
+            let number = Console.ReadLine()
+            printfn "Список его имен: "
+            List.iter (printfn "%s" << fst) <| List.filter (fun x -> snd x = number) dictionary
+            userInterface dictionary
+        | 5 ->
+            printfn "Вся база: "
+            List.iter (fun x -> printfn "%s: %s" (fst x) (snd x)) dictionary
+            userInterface dictionary
+        | 6 -> 
+            use writer = new StreamWriter(File.OpenWrite("Dictionary.txt"))
+            List.iter (fun (x, y) -> writer.WriteLine(x + " " + y)) dictionary
+            writer.Close()
+            printfn "Сохранено в файл"
+            userInterface dictionary     
+        | 7 -> 
+            try
+                use reader = new StreamReader(File.OpenRead("Dictionary.txt"))
+                let rec newDictionary acc = 
+                    if (not reader.EndOfStream) then 
+                        let pair = reader.ReadLine().Split[| |]
+                        newDictionary ((pair.[0], pair.[1])::acc)
+                    else
+                        acc
+                let newDict = newDictionary [] 
+                reader.Close()
+                printfn "Загружено из файла"
+                userInterface newDict
+            with
+            | :? FileNotFoundException ->
+                printfn "Файл не найден"
+                userInterface dictionary 
+            | _ ->
+                printfn "Ошибка чтения"
+                userInterface dictionary
+        | _ -> 
+            printfn "Заходите ещё"
         
 [<EntryPoint>]
 userInterface []

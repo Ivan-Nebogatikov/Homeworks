35aef7dc1e38f90a0d8f0e63e8c172d60be2956a
Ivan <moolush671@gmail.com>
2017-04-28 20:14:39 +0300

    Added solution for Homework 9

---
+++ NextHomework/Homework9/Task1/Program.fs
@@ -0,0 +1,24 @@
+﻿module Program
+
+open System.Threading
+open System
+
+/// Class for CountdownEvent. Takes positive integer
+type MyCountdownEvent(cnt) = 
+    do if cnt <= 0 then
+        raise (ArgumentOutOfRangeException("Аргумент должен быть положительным!"))
+    let event = new ManualResetEvent(false)
+    let mutable count = cnt
+
+    /// Locking the thread
+    member this.Wait () = 
+        if count <= 0 then
+            raise (ArgumentOutOfRangeException("Нельзя остановить поток, если значение = 0"))
+        event.WaitOne() |> ignore
+
+    /// Decreasing counter and realising thread if counter = 0
+    member this.Signal () = 
+        count <- max (count - 1) 0
+        if count = 0 then
+            event.Set() |> ignore
+    
---
+++ NextHomework/Homework9/Task1/Tests.fs
@@ -0,0 +1,92 @@
+﻿module Tests
+
+open FsUnit
+open NUnit.Framework
+open System.Threading
+open Program
+
+[<Test>]
+let ``Test with simple CountdownEvent should finished corectly`` () = 
+    let event = MyCountdownEvent(2)  
+    let mutable counter = 0
+    Async.Start(
+        async{
+            counter <- counter + 1
+            event.Wait()
+            counter <- counter + 1
+        }
+    )
+    Thread.Sleep 100
+    event.Signal()
+    Thread.Sleep 100
+    event.Signal()
+    Thread.Sleep 100
+    counter |> should equal 2
+
+    
+[<Test>]
+let ``Test with simple CountdownEvent should break async thread at Wait`` () = 
+    let event = new MyCountdownEvent(2)  
+    let mutable counter = 0
+    Async.Start(
+        async{
+            counter <- counter + 1
+            event.Wait()
+            counter <- counter + 1
+        }
+    )
+    Thread.Sleep 100
+    event.Signal()
+    Thread.Sleep 100
+    counter |> should equal 1
+
+    
+[<Test>]
+let ``Test with 2 CountdownEvents`` () = 
+    let event = new MyCountdownEvent(3)  
+    let mutable counter = 0
+    Async.Start(
+        async{
+            event.Wait()
+            counter <- counter + 5
+        }
+    )
+    Async.Start(
+        async{
+            event.Signal()
+            event.Wait()
+            counter <- counter + 4
+        }
+    )
+    Thread.Sleep 100
+    event.Signal()
+    Thread.Sleep 100
+    event.Signal()
+    Thread.Sleep 100
+    counter |> should equal 9
+
+    
+[<Test>]
+let ``Test with race condition`` () = 
+    let event = new MyCountdownEvent(3)  
+    let mutable counter = 0
+    Async.Start(
+        async{
+            event.Signal()
+            event.Wait()
+            event.Signal()
+            counter <- counter + 5
+        }
+    )
+    Async.Start(
+        async{
+            event.Signal()
+            event.Wait()
+            event.Signal()
+            counter <- counter + 4
+        }
+    )
+    Thread.Sleep 100
+    event.Signal()
+    Thread.Sleep 100
+    counter |> should equal 9
